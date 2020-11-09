a321b5d0f174eafb8350f288d28f9058dbc100d4
Stovencrantz <sknapp143@gmail.com>
2020-11-09 12:41:31 -0500

    Added code to set the navbar tabs to be active if the user is on that particular page

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function Navbar() {
 
+    const [ activeNav, setActiveNav ] = useState("");
+
+    function setActivePill(event) {
+        let nav = document.getElementById("searchNavItem").textContent;
+        setActiveNav(nav);
+        console.log("Active pill: ", activeNav);
+
+    }
+
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">Google Books</a>
+            <a className="navbar-brand">Google Books</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse id="navbarNav>
                 <ul className="navbar-nav">
-                    <li className="nav-item">
+                    <li className="nav-item" id="searchNavItem"  active={ activeNav === "Search" } onClick={setActivePill}>
                         <a className="nav-link" href="/">Search</a>
                     </li>
-                    <li>
+                    <li className="nav-item" id="savedNavItem"  active={ activeNav === "Saved" } onClick={setActivePill}>
                         <a className="nav-link" href="/saved">Saved</a>
                     </li>
                 </ul>
